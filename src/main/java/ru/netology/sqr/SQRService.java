4e8825df1fb0ae6aff442305d57e9b648d97d5eb
Tigradcom <tigradcom2@gmail.com@>
2022-06-29 00:30:40 +0400

    Add Files

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,13 @@
+package ru.netology.sqr;
+public class SQRService {
+
+    public int calcCount(int min, int max) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (min <= i*i & i*i <= max) {
+                count++;
+                }
+            }
+        return count;
+    }
+}
