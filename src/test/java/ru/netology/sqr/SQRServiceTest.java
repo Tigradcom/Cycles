47be7de1778544fe812a5d81ac7af29cde8cc99d
Tigradcom <tigradcom2@gmail.com@>
2022-07-05 22:13:35 +0400

    reformat Code

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology.sqr;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -8,48 +10,60 @@ public class SQRServiceTest {
     @Test
     public void shouldCalc() {
         SQRService service = new SQRService();
+
         int actual = service.calcCount(200, 300);
         int expected = 3;
+
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCalcMin() {
         SQRService service = new SQRService();
+
         int actual = service.calcCount(99, 101);
         int expected = 1;
+
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCalcMax() {
         SQRService service = new SQRService();
+
         int actual = service.calcCount(9_800, 9_802);
         int expected = 1;
+
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCalcMinOutside() {
         SQRService service = new SQRService();
+
         int actual = service.calcCount(0, 99);
         int expected = 0;
+
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCalcMaxOutside() {
         SQRService service = new SQRService();
+
         int actual = service.calcCount(9_802, 1_000_000);
         int expected = 0;
+
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldCalcNegativeNumber() {
         SQRService service = new SQRService();
+
         int actual = service.calcCount(-200, -300);
         int expected = 0;
+
         assertEquals(expected, actual);
     }
 }
